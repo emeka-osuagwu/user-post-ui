6336a5aeed684a3e701710cf82f3f95358856036
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2024-11-29 13:22:14 +0100

    update

--- react-frontend/src/setupTests.js
+++ react-frontend/src/setupTests.js
@@ -1,15 +1,19 @@
 // src/setupTests.js
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 import { setupServer } from 'msw/node';
 import { handlers } from './mocks/handlers';
 import '@testing-library/jest-dom';
 
+// Initialize the MSW server
 const server = setupServer(...handlers);
 
-// Start the mock server before tests
+// Start the server before tests
 beforeAll(() => server.listen());
-
 // Reset handlers after each test
 afterEach(() => server.resetHandlers());
-
-// Stop the mock server after tests
+// Close the server after tests
 afterAll(() => server.close());
